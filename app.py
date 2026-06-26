daf3328fd07951fa337ea4056129941b61798499
Kravion Williams <kwilliams56@crimson.ua.edu>
2026-06-25 21:59:18 -0500

    Add NBA player search

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from nba_api.stats.static import players
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    player_info = None
+    error = None
+
+    if request.method == "POST":
+        player_name = request.form.get("player_name")
+
+        matching_players = players.find_players_by_full_name(player_name)
+
+        if matching_players:
+            player_info = matching_players[0]
+        else:
+            error = "Player not found. Try another name."
+
+    return render_template("index.html", player=player_info, error=error)
 
 if __name__ == "__main__":
     app.run(debug=True)
